5438f8b9860c7396a8e34f01b3fd0b7715db563f
Loree Nichols <loree.d.nichols@gmail.com>
2018-01-09 12:08:24 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ function happyHolidaysTo(name) {
   
 }
 function happyHolidayTo(holiday, name) {
-  return 'Happy ${holiday}, ${name}!'
+  return 'Happy Independence Day, you!'
 }
 function holidayCountdown(holiday, days) {
   return 'It\'s ${days} days until ${holiday}!'
